36ad324380eafffdd888874e85d6f84237970734
AlexAvVilla <alex.avila3232@gmail.com>
2023-05-07 17:50:47 -0600

    Se añade el programa

---
+++ src/main.java
@@ -0,0 +1,28 @@
+
+import Vista.frmPrincipal;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+/**
+ *
+ * @author Alex
+ */
+public class main {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    
+    
+    
+    
+    public static void main(String[] args) {
+        frmPrincipal VistaPrincipal = new frmPrincipal();
+        VistaPrincipal.setVisible(true);
+    }
+    
+}
