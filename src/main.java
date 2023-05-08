b17ced68a683471b0533ae8e45a4a1d8f96a8668
trstr <trstr@DESKTOP-395GO8Q>
2023-05-08 01:40:54 -0600

    Todo el metodo terminado

--- src/main.java
+++ src/main.java
@@ -1,25 +1,7 @@
-
 import Vista.frmPrincipal;
 
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
-
-/**
- *
- * @author Alex
- */
 public class main {
 
-    /**
-     * @param args the command line arguments
-     */
-    
-    
-    
-    
-    
     public static void main(String[] args) {
         frmPrincipal VistaPrincipal = new frmPrincipal();
         VistaPrincipal.setVisible(true);
